9e6b3bd0b6687de1b63396200c11f6810bbc2ea2
navadeepak <128801710+navadeepak@users.noreply.github.com>
2023-11-11 13:32:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function mob_no(){
+    document.getElementById("in").innerHTML="+91 76038 57110";
+}
+function mail_id(){
+    document.getElementById("in").innerHTML="navadeepakaswin007@gmail.com";
+}
